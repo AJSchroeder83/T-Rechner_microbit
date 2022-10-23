221ed9a68beea5bdbbcbee99c56884284c11127c
AJSchroeder83 <116520195+AJSchroeder83@users.noreply.github.com>
2022-10-23 23:01:53 +0200

    A Taste waehlt die Zahl und B Taste waehlt die Rechenart , Schleifen hinzugefügt, Serielle hinzugefügt

--- main.ts
+++ main.ts
@@ -1,23 +1,77 @@
 input.onButtonPressed(Button.A, function () {
-    ZahlA = ZahlA + 1
-    basic.showNumber(ZahlA)
+    if (Rechenart == 0) {
+        ZahlA = ZahlA + 1
+        basic.showNumber(ZahlA)
+        serial.writeLine("" + (ZahlA))
+    }
+    if (Rechenart >= 1) {
+        ZahlB = ZahlB + 1
+        basic.showNumber(ZahlB)
+        serial.writeLine("" + (ZahlB))
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    ZahlSumme = ZahlA + ZahlB
-    basic.showNumber(ZahlSumme)
+    if (Rechenart == 1) {
+        basic.showString("+")
+        basic.pause(1000)
+        ZahlSumme = ZahlA + ZahlB
+    }
+    if (Rechenart == 2) {
+        basic.showString("-")
+        basic.pause(1000)
+        ZahlSumme = ZahlA - ZahlB
+    }
+    if (Rechenart == 3) {
+        basic.showString("*")
+        basic.pause(500)
+        ZahlSumme = ZahlA * ZahlB
+    }
+    if (Rechenart == 4) {
+        basic.showString(":")
+        basic.pause(1000)
+        ZahlSumme = ZahlA / ZahlB
+    } else {
+        ZahlA = 0
+        ZahlB = 0
+        Rechenart = 0
+    }
+    serial.writeLine("Ergebnis = ")
+    serial.writeNumber(ZahlSumme)
+    for (let index = 0; index < 3; index++) {
+        basic.showNumber(ZahlSumme)
+    }
     basic.pause(1000)
-    ZahlA = 0
-    ZahlB = 0
 })
 input.onButtonPressed(Button.B, function () {
-    ZahlB = ZahlB + 1
-    basic.showNumber(ZahlB)
+    Rechenart = Rechenart + 1
+    if (Rechenart == 1) {
+        basic.showString("+")
+        serial.writeLine("+ Modus")
+    }
+    if (Rechenart == 2) {
+        basic.showString("-")
+        serial.writeLine("- Modus")
+    }
+    if (Rechenart == 3) {
+        basic.showString("*")
+        serial.writeLine("x Modus")
+    }
+    if (Rechenart == 4) {
+        basic.showString(":")
+        serial.writeLine(": Modus")
+    }
+    if (Rechenart >= 5) {
+        Rechenart = 0
+        basic.showIcon(IconNames.Surprised)
+        serial.writeLine("Beginn von Vorn")
+    }
 })
-let ZahlB = 0
 let ZahlSumme = 0
+let ZahlB = 0
 let ZahlA = 0
-basic.showString("Start Test1")
-serial.writeLine("Starte Test1")
+let Rechenart = 0
+basic.showString("T-Rechner V1.1")
+serial.writeLine("Starte Taschenrechner V1.1")
 basic.forever(function () {
 	
 })
